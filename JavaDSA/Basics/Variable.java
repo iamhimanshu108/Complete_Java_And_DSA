e16b147f7b9f9527c0d133f9ba5e8e04de05b8f1
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2025-06-03 23:14:31 +0530

    Variables

---
+++ JavaDSA/Basics/Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+    public static void main(String[] args) {
+        int x =15;
+        int y= 10;
+        int sum = x+y;
+
+        System.out.println(sum);
+
+        int prd=x*y;
+        System.out.println(prd);
+
+        int v1 = x/y;
+        int v2 = y/x;
+        int v3 = x %y;
+
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(v3);
+    }
+}
