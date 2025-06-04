4b684742c374b905824f30958c4bb41725d977fe
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2025-06-04 20:52:01 +0530

    Loops

---
+++ JavaDSA/Basics/L8_Loops.java
@@ -0,0 +1,18 @@
+public class L8_Loops {
+    
+    
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 9) {
+            System.out.println(i);
+            i = i + 1;  // Increment i properly
+        }
+ 
+        for(int x=2; x<=15; x++){
+            System.out.println(x);
+        }
+        
+        System.out.println("Done");
+    }
+}
