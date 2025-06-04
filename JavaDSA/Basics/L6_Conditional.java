99e5e5d438c9b8c48db60cc09026923f552bb876
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2025-06-04 20:42:29 +0530

    Conditional

---
+++ JavaDSA/Basics/L6_Conditional.java
@@ -0,0 +1,15 @@
+public class L6_Conditional {
+    public static void main(String[] args) {
+        int marks = 78;
+
+        if (marks >90) {
+            System.out.println("Excellent");
+        }
+        if (marks > 80 &&  marks<=90) {
+            System.out.println("Good");
+        }
+        if (marks >70 &&  marks<=80) {
+            System.out.println("fair");
+        }
+    }
+}
