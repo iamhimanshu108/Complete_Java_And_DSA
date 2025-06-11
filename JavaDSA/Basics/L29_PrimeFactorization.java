bc499fb7811164585556ebf2aa2a7237e72ced47
Himanshu Kumar (Yadav) <110648951+iamhimanshu108@users.noreply.github.com>
2025-06-11 14:47:39 +0000

    PrimeFactorization

--- JavaDSA/Basics/L29_PrimeFactorization.java
+++ JavaDSA/Basics/L29_PrimeFactorization.java
@@ -8,12 +8,12 @@ public class L29_PrimeFactorization {
         for(int div = 2; div * div <=n; div++){
             while(n % div ==0){
                 n = n/div;
-                System.out.println(div);
+                System.out.print(div);
             }
         }
 
         if(n !=1){
-            System.out.println(n);
+            System.out.print(n);
         }
     }
 }
